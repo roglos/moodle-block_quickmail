43a2332c5c44807aa3e840e9055dbc78a934619c
roglos <roelmann@glos.ac.uk>
2016-08-24 10:28:23 +0100

    Initial commit

---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+
+// Written at Louisiana State University
+//
+$plugin->version = 2016080100;
+$plugin->requires = 2013051400;
+$plugin->release = "v1.5.1"; // Richard's release
+$plugin->maturity = MATURITY_STABLE;
+$plugin->component = 'block_quickmail'; // Added in to make block compatible with 3.1
